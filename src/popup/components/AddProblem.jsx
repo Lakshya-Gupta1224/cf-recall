2f4f7a1c0627bcd599564f9525678ee7701ba7f4
Lakshya-Gupta1224 <84818894+Lakshya-Gupta1224@users.noreply.github.com>
2025-12-23 06:18:38 +0530

    commented out AddProblem.jsx and TodayList.jsx
    weren't being used because became too heavy
    to maintian, migrated all code
    and functions to smaller components
    applying SOLID principles

--- src/popup/components/AddProblem.jsx
+++ src/popup/components/AddProblem.jsx
@@ -1,66 +1,66 @@
-import { getProblems, saveProblems } from "../../../public/utils/storage";
-import { todayISO, addDays } from "../../../public/utils/date";
-export const SPACED_INTERVALS = [1, 3, 7, 14, 30, 60];
+// import { getProblems, saveProblems } from "../../../public/utils/storage";
+// import { todayISO, addDays } from "../../../public/utils/date";
+// export const SPACED_INTERVALS = [1, 3, 7, 14, 30, 60];
 
-export default function AddNewProblem(problemToAdd) {
-  const { problemId } = problemToAdd;
-  let problems = getProblems();
-  if (!problems) {
-    problems = {};
-  }
+// export default function AddNewProblem(problemToAdd) {
+//   const { problemId } = problemToAdd;
+//   let problems = getProblems();
+//   if (!problems) {
+//     problems = {};
+//   }
 
-  if (problems[problemId]) {
-    alert("Problem already added!");
-    return;
-  }
+//   if (problems[problemId]) {
+//     alert("Problem already added!");
+//     return;
+//   }
 
-  const today = todayISO();
-  const d = addDays(todayISO, 1);
-  problemToAdd = {
-    ...problemToAdd,
-    nextReviewDate: d,
-    history: [{ date: today, action: "added" }],
-  };
-  problems[problemId] = problemToAdd;
-  saveProblems(problems, () => alert("Problem added to CF Recall"));
+//   const today = todayISO();
+//   const d = addDays(todayISO, 1);
+//   problemToAdd = {
+//     ...problemToAdd,
+//     nextReviewDate: d,
+//     history: [{ date: today, action: "added" }],
+//   };
+//   problems[problemId] = problemToAdd;
+//   saveProblems(problems, () => alert("Problem added to CF Recall"));
 
-  // chrome.storage.local.set({ problems }, () => {
-  //   alert("Problem added to CF Recall");
-  // });
-  // chrome.storage.local.get(["problems"], (result) => {
-  //   problems[problemId] = {
-  //     id: problemId,
-  //     name: problemName,
-  //     url: window.location.href,
-  //     intervalIndex: 0,
-  //     nextReviewDate: d,
-  //     history: [{ date: today, action: "added" }],
-  //     status: "pending",
-  //   };
-  // });
+//   // chrome.storage.local.set({ problems }, () => {
+//   //   alert("Problem added to CF Recall");
+//   // });
+//   // chrome.storage.local.get(["problems"], (result) => {
+//   //   problems[problemId] = {
+//   //     id: problemId,
+//   //     name: problemName,
+//   //     url: window.location.href,
+//   //     intervalIndex: 0,
+//   //     nextReviewDate: d,
+//   //     history: [{ date: today, action: "added" }],
+//   //     status: "pending",
+//   //   };
+//   // });
 
-  // async function addProblem() {
-  //   const problems = await getProblems();
-  //   const id = "1791F";
-  //   if (problems[id]) return alert("Problem already exists.");
-  //   const today = todayISO();
-  //   problems[id] = {
-  //     id,
-  //     name: "Range Update Point Query",
-  //     url: "https://codeforces.com/problemset/problem/1791/F",
-  //     intervalIndex: 0,
-  //     nextReviewDate: addDays(today, SPACED_INTERVALS[0]),
-  //     history: [{ date: today, action: "added" }],
-  //     status: "pending",
-  //   };
+//   // async function addProblem() {
+//   //   const problems = await getProblems();
+//   //   const id = "1791F";
+//   //   if (problems[id]) return alert("Problem already exists.");
+//   //   const today = todayISO();
+//   //   problems[id] = {
+//   //     id,
+//   //     name: "Range Update Point Query",
+//   //     url: "https://codeforces.com/problemset/problem/1791/F",
+//   //     intervalIndex: 0,
+//   //     nextReviewDate: addDays(today, SPACED_INTERVALS[0]),
+//   //     history: [{ date: today, action: "added" }],
+//   //     status: "pending",
+//   //   };
 
-  //   await saveProblems(problems);
-  //   alert("Problem added");
-  // }
+//   //   await saveProblems(problems);
+//   //   alert("Problem added");
+//   // }
 
-  // return (
-  //   <button className="button is-link is-small" onClick={addProblem}>
-  //     Add Sample Problem
-  //   </button>
-  // );
-}
+//   // return (
+//   //   <button className="button is-link is-small" onClick={addProblem}>
+//   //     Add Sample Problem
+//   //   </button>
+//   // );
+// }
--- src/popup/components/TodayList.jsx
+++ src/popup/components/TodayList.jsx
@@ -1,128 +1,128 @@
-import { useEffect, useState } from "react";
-import { getProblems, saveProblems } from "../../../public/utils/storage";
-import { todayISO, addDays } from "../../../public/utils/date";
-import { loadProblemPage } from "../../../public/utils/loadPage";
-export const SPACED_INTERVALS = [1, 3, 7, 14, 30, 60];
+// import { useEffect, useState } from "react";
+// import { getProblems, saveProblems } from "../../../public/utils/storage";
+// import { todayISO, addDays } from "../../../public/utils/date";
+// import { loadProblemPage } from "../../../public/utils/loadPage";
+// export const SPACED_INTERVALS = [1, 3, 7, 14, 30, 60];
 
-export default function TodayList() {
-  const [list, setList] = useState([]);
-  const [selectedDate, setSelectedDate] = useState(todayISO());
+// export default function TodayList() {
+//   const [list, setList] = useState([]);
+//   const [selectedDate, setSelectedDate] = useState(todayISO());
 
-  // async function load() {
-  //   const problems = await getProblems();
-  //   const today = todayISO();
-  //   console.log(today.toString());
-  //   const due = Object.values(problems).filter(
-  //     (p) => p.nextReviewDate <= today && p.status === "pending"
-  //   );
-  //   setList(due);
-  // }
+//   // async function load() {
+//   //   const problems = await getProblems();
+//   //   const today = todayISO();
+//   //   console.log(today.toString());
+//   //   const due = Object.values(problems).filter(
+//   //     (p) => p.nextReviewDate <= today && p.status === "pending"
+//   //   );
+//   //   setList(due);
+//   // }
 
-  async function loadForDate(date) {
-    const problems = await getProblems();
+//   async function loadForDate(date) {
+//     const problems = await getProblems();
 
-    const filtered = Object.values(problems).filter(
-      (p) => p.nextReviewDate === date && p.status === "pending"
-    );
+//     const filtered = Object.values(problems).filter(
+//       (p) => p.nextReviewDate === date && p.status === "pending"
+//     );
 
-    setList(filtered);
-  }
+//     setList(filtered);
+//   }
 
-  async function markReviewed(problem) {
-    const problems = await getProblems();
-    const nextIndex = Math.min(
-      problem.intervalIndex + 1,
-      SPACED_INTERVALS.length - 1
-    );
-    const today = todayISO();
-    problems[problem.id] = {
-      ...problem,
-      intervalIndex: nextIndex,
-      nextReviewDate: addDays(today, SPACED_INTERVALS[nextIndex]),
-      history: [...problem.history, { date: today, action: "reviewed" }],
-    };
-    await saveProblems(problems);
-    loadForDate(selectedDate);
-  }
+//   async function markReviewed(problem) {
+//     const problems = await getProblems();
+//     const nextIndex = Math.min(
+//       problem.intervalIndex + 1,
+//       SPACED_INTERVALS.length - 1
+//     );
+//     const today = todayISO();
+//     problems[problem.id] = {
+//       ...problem,
+//       intervalIndex: nextIndex,
+//       nextReviewDate: addDays(today, SPACED_INTERVALS[nextIndex]),
+//       history: [...problem.history, { date: today, action: "reviewed" }],
+//     };
+//     await saveProblems(problems);
+//     loadForDate(selectedDate);
+//   }
 
-  async function deleteProblem(problemId) {
-    chrome.runtime.sendMessage(
-      {
-        type: "DELETE_PROBLEM",
-        payload: { problemId },
-      },
-      (response) => {
-        if (response?.status === "SUCCESS") {
-          console.log("Problem deleted");
-          loadForDate(selectedDate);
-        } else {
-          console.log("Problem not found");
-        }
-      }
-    );
-  }
+//   async function deleteProblem(problemId) {
+//     chrome.runtime.sendMessage(
+//       {
+//         type: "DELETE_PROBLEM",
+//         payload: { problemId },
+//       },
+//       (response) => {
+//         if (response?.status === "SUCCESS") {
+//           console.log("Problem deleted");
+//           loadForDate(selectedDate);
+//         } else {
+//           console.log("Problem not found");
+//         }
+//       }
+//     );
+//   }
 
-  useEffect(() => {
-    loadForDate(selectedDate);
+//   useEffect(() => {
+//     loadForDate(selectedDate);
 
-    const listener = () => loadForDate(selectedDate);
-    chrome.storage.onChanged.addListener(listener);
+//     const listener = () => loadForDate(selectedDate);
+//     chrome.storage.onChanged.addListener(listener);
 
-    return () => chrome.storage.onChanged.removeListener(listener);
-  }, []);
+//     return () => chrome.storage.onChanged.removeListener(listener);
+//   }, []);
 
-  useEffect(() => {
-    loadForDate(selectedDate);
-    window.addEventListener("focus", loadForDate);
+//   useEffect(() => {
+//     loadForDate(selectedDate);
+//     window.addEventListener("focus", loadForDate);
 
-    return () => window.removeEventListener("focus", loadForDate);
-  }, []);
+//     return () => window.removeEventListener("focus", loadForDate);
+//   }, []);
 
-  return (
-    <div>
-      {/* <h2 className="subtitle is-6">Today's Problems</h2> */}
-      <div className="field">
-        <label htmlFor="inputDatePicker" className="label">
-          Select date
-        </label>
-        <input
-          id="inputDatePicker"
-          type="date"
-          min={todayISO()}
-          className="input is-small mb-3"
-          value={selectedDate}
-          onChange={(e) => {
-            const date = e.target.value;
-            setSelectedDate(date);
-            loadForDate(date);
-          }}
-        />
-      </div>
-      {list.length === 0 && <p>No problems for this day.</p>}
-      {list.map((p) => (
-        <div key={p.id} className="box">
-          {p.name}
-          <hr />
-          <button
-            className="button mr-1 is-info is-small"
-            onClick={() => loadProblemPage(p.url)}
-          >
-            Revisit
-          </button>
-          <button
-            className="button mr-1 is-success is-small"
-            onClick={() => markReviewed(p)}
-          >
-            Mark Reviewed
-          </button>
-          <button
-            className="button is-small is-danger"
-            onClick={() => deleteProblem(p.id)}
-          >
-            Delete
-          </button>
-        </div>
-      ))}
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       {/* <h2 className="subtitle is-6">Today's Problems</h2> */}
+//       <div className="field">
+//         <label htmlFor="inputDatePicker" className="label">
+//           Select date
+//         </label>
+//         <input
+//           id="inputDatePicker"
+//           type="date"
+//           min={todayISO()}
+//           className="input is-small mb-3"
+//           value={selectedDate}
+//           onChange={(e) => {
+//             const date = e.target.value;
+//             setSelectedDate(date);
+//             loadForDate(date);
+//           }}
+//         />
+//       </div>
+//       {list.length === 0 && <p>No problems for this day.</p>}
+//       {list.map((p) => (
+//         <div key={p.id} className="box">
+//           {p.name}
+//           <hr />
+//           <button
+//             className="button mr-1 is-info is-small"
+//             onClick={() => loadProblemPage(p.url)}
+//           >
+//             Revisit
+//           </button>
+//           <button
+//             className="button mr-1 is-success is-small"
+//             onClick={() => markReviewed(p)}
+//           >
+//             Mark Reviewed
+//           </button>
+//           <button
+//             className="button is-small is-danger"
+//             onClick={() => deleteProblem(p.id)}
+//           >
+//             Delete
+//           </button>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// }
